bbecfc90e680151d82c413695d325dc080813198
Moulik-23 <moulikzinzala912@gmail.com>
2025-01-21 18:59:47 +0530

    Data Transformatio done

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -45,3 +45,13 @@ class DataValidationConfig:
             trainning_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
             trainning_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
         )
+
+class DataTransformationConfig:
+     def __init__(self,training_pipeline_config:TrainnigPipelineConfig):
+        self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,trainning_pipeline.DATA_TRANSFORMATION_DIR_NAME )
+        self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,trainning_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            trainning_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  trainning_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            trainning_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, trainning_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            trainning_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
